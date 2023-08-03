52fe504216499225ec8d05c4fc523c8af28b7de7
Temurbek Rakhimkuliev <56798388+timurxboy@users.noreply.github.com>
2023-08-03 11:38:53 +0500

    Create AddTwoNumbers.py
    Example 1 
    Calculate the sum of two numbers entered by the user..
    
    Example 2
    In this program, we asked the user to enter two numbers, and this program displays the sum of the two numbers entered by the user.
    We use the built-in input() function to get input data. Since the input() function returns a string, we convert the string to a number using the float() function. Then the numbers are summed up.
    
    Example 3
    Alternatively, we can perform this addition in a single statement without using any variables.

---
+++ AddTwoNumbers.py
@@ -0,0 +1,26 @@
+# EXAMPLE 1
+# This program adds two numbers
+num1 = 13.5
+num2 = 9.7
+
+# Add two numbers
+summ = num1 + num2
+
+# Display the sum
+print(f"The sum of {num1} and {num2} is {sum}")
+
+
+# EXAMPLE 2
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+summ = float(num1) + float(num2)
+
+# Display the sum
+print(f"The sum of {num1} and {num2} is {summ}")
+
+
+# EXAMPLE 3
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
