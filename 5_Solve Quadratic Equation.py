7481d19e69e677b8baecf27bf5eb08e19fe6e834
Temurbek Rakhimkuliev <56798388+timurxboy@users.noreply.github.com>
2023-08-14 16:32:57 +0500

    5_Solve Quadratic Equation.py
    We have imported the cmath module to perform complex square root. First, we calculate the discriminant and then find the two solutions of the quadratic equation.
    
    You can change the value of a, b and c in the above program and test this program.

---
+++ 5_Solve Quadratic Equation.py
@@ -0,0 +1,29 @@
+"""
+The standard form of a quadratic equation is:
+ax2 + bx + c = 0, where
+a, b and c are real numbers and
+a ≠ 0
+"""
+
+"""
+The solutions of this quadratic equation is given by:
+(-b ± (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+"""
+
+# Solve the quadratic equation ax**2 + bx + c = 0
+
+# import complex math module
+import cmath
+
+a = 1
+b = 5
+c = 6
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1,sol2))
